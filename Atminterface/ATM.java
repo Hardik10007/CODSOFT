862b76bd7173d275d6ea45ed6f82abb97d4dd07a
Hardik10007 <140045725+Hardik10007@users.noreply.github.com>
2023-08-02 12:03:12 +0530

    Add files via upload

---
+++ Atminterface/ATM.java
@@ -0,0 +1,33 @@
+package Atminterface;
+public class ATM {
+    private double balance;
+    private double depositamount;
+    private double withdrawnamount;
+
+    public ATM(){
+
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getDepositamount() {
+        return depositamount;
+    }
+
+    public void setDepositamount(double depositamount) {
+        this.depositamount = depositamount;
+    }
+
+    public double getWithdrawnamount() {
+        return withdrawnamount;
+    }
+
+    public void setWithdrawnamount(double withdrawnamount) {
+        this.withdrawnamount = withdrawnamount;
+    }
+}
---
+++ Atminterface/Atmoperationsinter.java
@@ -0,0 +1,7 @@
+package Atminterface;
+public interface Atmoperationsinter {
+    public void viewbalance();
+    public void withdraw(double withdrawamount);
+    public void deposit(double depositamount);
+    public void viewstatement();
+}
