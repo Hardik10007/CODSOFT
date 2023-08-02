7d32c168688364204c47e9c3152a45ba9c8416b2
Hardik10007 <140045725+Hardik10007@users.noreply.github.com>
2023-08-02 11:57:30 +0530

    Delete ATM interface directory

--- ATM interface/Atmoperationsinter.java
+++
@@ -1,7 +0,0 @@
-package ATM interface ;
-public interface Atmoperationsinter {
-    public void viewbalance();
-    public void withdraw(double withdrawamount);
-    public void deposit(double depositamount);
-    public void viewstatement();
-}
